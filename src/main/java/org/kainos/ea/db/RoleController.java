bd2a2509a904770401ea60bc786dfabcfa009c47
Odhran <odhran.hague@kainos.com>
2023-12-05 11:53:11 +0000

    Finished API reop

---
+++ src/main/java/org/kainos/ea/db/RoleController.java
@@ -0,0 +1,14 @@
+package org.kainos.ea.db;
+
+import io.swagger.annotations.Api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Api("JobForge API test")
+@Path("/api")
+public class RoleController {
+}
