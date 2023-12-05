6b092276ab8cffde9b1e733a859242634a1844bd
UserConor <conor.trainor@kainos.com>
2023-12-05 21:25:51 +0000

    Moved placeholder files.

--- src/main/java/org/kainos/ea/db/DatabaseConnector.java
+++ src/main/java/org/kainos/ea/db/DatabaseConnector.java
@@ -10,12 +10,15 @@ public class DatabaseConnector {
 
     private static Connection conn;
 
+
     public static Connection getConnection() throws SQLException {
         String user, password, host, name;
 
-        if (conn != null && !conn.isClosed()) { return conn;}
+        if (conn != null && !conn.isClosed()) {
+            return conn;
+        }
 
-        try(FileInputStream propsStream = new FileInputStream("db.properties")){
+        try(FileInputStream propsStream = new FileInputStream("db.properties")) {
 
             Properties props = new Properties();
             props.load(propsStream);
@@ -26,16 +29,18 @@ public class DatabaseConnector {
             name = props.getProperty("name");
 
             if (user == null || password == null || host == null)
-                throw new IllegalArgumentException("Properties file must exist "+
+                throw new IllegalArgumentException("Properties file must exist " +
                         "and must contain user, password, name and host properties.");
 
             conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" + name + "?useSSL=false", user, password);
             return conn;
 
-
-        } catch (Exception e){
+        } catch (Exception e) {
             System.err.println(e.getMessage());
+        } finally {
+            System.out.println("I will always run!");
         }
+
         return null;
     }
 }
