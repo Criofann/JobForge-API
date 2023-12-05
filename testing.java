4e9c7dd6cd9c3b8aff4b0604a818cc62eb892d75
Joseph-Magee <joseph.magee@kainos.com>
2023-12-05 14:05:58 +0000

    acommit

--- testing.java
+++ testing.java
@@ -9,18 +9,9 @@ public class Example {
                 System.out.println("x is equal to 2");
                 break;
             default:
-                
+                System.out.println("x is not equal to 1 or 2");
+                break;
         }
     }
 }
-/* UnusedVariable: The variable args is 
-declared but never used.
-sEmptyBlock: The default block is empty. It’s
-generally considered a bad practice to have empty 
-blocks because they can lead
- to bugs and make the code harder to read.
-MissingSwitchDefault: The switch statement 
-doesn’t have a default case. It’s
- generally considered a good practice to 
-include a default case in switch statements to 
-handle unexpected values.*/
+
