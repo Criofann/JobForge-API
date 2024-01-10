717f69a5e532e3787a3bbeff4f051d0cfcf8ff7f
Joseph-Magee <joseph.magee@kainos.com>
2024-01-10 14:41:21 +0000

    delete-uneeded

--- src/main/java/org/kainos/ea/client/FailedToCreateJobFamilyException.java
+++
@@ -1,9 +0,0 @@
-package org.kainos.ea.client;
-
-public class FailedToCreateJobFamilyException extends Throwable {
-    @Override
-    public String getMessage() {
-        return "Failed to create job";
-    }
-}
-
--- src/main/java/org/kainos/ea/client/InvalidJobFamilyException.java
+++
@@ -1,7 +0,0 @@
-package org.kainos.ea.client;
-
-public class InvalidJobFamilyException extends Throwable {
-    public InvalidJobFamilyException(String error) {
-        super(error);
-    }
-}
